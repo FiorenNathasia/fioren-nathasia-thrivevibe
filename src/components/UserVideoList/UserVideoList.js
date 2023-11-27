ea73ae75ab14d21d3689c0daa85dceb91b6dd3ce
FiorenNathasia <fiorennathasia@gmail.com>
2023-11-27 17:31:25 +0000

    Finished styling login page.

--- src/components/UserVideoList/UserVideoList.js
+++ src/components/UserVideoList/UserVideoList.js
@@ -1,18 +1,48 @@
 import "./UserVideoList.scss";
 import UserVideoCard from "../UserVideoCard/UserVideoCard";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Box from "@mui/material/Box";
 
 export default function UserVideoList({ videos }) {
   return (
     <>
       <div className="videolist">
-        {videos.map((video) => (
-          <UserVideoCard
-            key={video.id}
-            id={video.id}
-            url={video.url}
-            prompt={video.prompt}
-          />
-        ))}
+        <Box
+          sx={{
+            border: "none",
+            height: "500px",
+            width: "100%", // Use a percentage width to make it responsive
+            overflow: "auto", // Use auto instead of scroll to show scrollbar only when necessary
+            boxShadow: 3,
+            borderRadius: "10px",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+          }}
+        >
+          <List
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
+          >
+            {videos.map((video) => (
+              <ListItem>
+                {" "}
+                <UserVideoCard
+                  key={video.id}
+                  id={video.id}
+                  url={video.url}
+                  prompt={video.prompt}
+                  sx={{ flexBasis: "calc(25% - 20px)", margin: "10px" }}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </div>
     </>
   );
